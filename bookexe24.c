36cc406b9b3762f642ff07f6eb97c4b190bff54e
VPS2212 <126179069+VPS2212@users.noreply.github.com>
2023-03-22 21:56:20 +0530

    Book Qustion solved code -Ternary operator
    write a code get any 2 input number, if first number greater then second number print the different of the number, else print the sum of the number . i study the ternary operator use , and code it myself.

---
+++ bookexe24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the number a and b\n");
+	scanf("%d%d",&a,&b);
+	int c=(a>b)? printf("A is greater then B:%d\n",c=a-b): printf("A is Less then Value of B%d\n",c=a+b);
+	return 0;
+}
+	
